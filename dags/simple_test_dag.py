f81aa3e46903d5304dd1eb578a3cd497522cc7f3
Pawel Kwasniewski <pawel.kwasniewski@dnb.no>
2025-04-23 10:27:23 +0200

    Switching to v3

--- dags/simple_test_dag.py
+++ dags/simple_test_dag.py
@@ -1,5 +1,6 @@
 from pendulum import datetime, duration
-from airflow.decorators import dag, task
+from airflow import DAG
+from airflow.sdk import task
 
 default_args = {
     'owner': 'airflow',
@@ -7,20 +8,17 @@ default_args = {
     'retry_delay': duration(minutes=5),
 }
 
-@dag(
+with DAG(
     dag_id='hello_world_dag',
     default_args=default_args,
     description='A simple Hello World DAG using TaskFlow API',
-    schedule_interval='* * * * *',  # Every minute
+    schedule='* * * * *',  # Every minute
     start_date=datetime(2023, 1, 1, tz="Europe/Oslo"),
     catchup=False,
     tags=['example'],
-)
-def hello_world_dag():
+) as dag:
     @task
     def hello_world():
         print("Hello, world!")
 
     hello_world()
-
-dag = hello_world_dag()
